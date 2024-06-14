c4f692f3fe33880165f0c8705b16430352b9aae7
TristandeMesmay <2415668@ucc.edu.ar>
2024-06-14 20:59:57 +0200

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -1,32 +1,29 @@
-document.getElementById('imcForm').addEventListener('submit', function (event) {
+document.getElementById('imcForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var peso = document.getElementById('input_peso').value.trim();
-    var talla = document.getElementById('input_talla').value.trim();
+    let peso = document.getElementById('input_peso').value.trim();
+    let talla = document.getElementById('input_talla').value.trim();
 
     document.getElementById('peso_error').style.display = 'none';
     document.getElementById('talla_error').style.display = 'none';
 
-
-    if (!validateNumber(peso)) {
+    if (!validateNumber(peso, 5, 650)) {
         document.getElementById('peso_error').style.display = 'inline';
         return;
     }
 
-
-    if (!validateNumber(talla)) {
+    if (!validateNumber(talla, 30, 250)) {
         document.getElementById('talla_error').style.display = 'inline';
         return;
     }
 
-
-    var pesoNum = parseFloat(peso);
-    var tallaNum = parseFloat(talla) / 100; // Convertir a metros
-    var imc = pesoNum / (tallaNum * tallaNum);
+    let pesoNum = parseFloat(peso);
+    let tallaNum = parseFloat(talla) / 100; // Convertir à mètres
+    let imc = pesoNum / (tallaNum * tallaNum);
     imc = Math.round(imc);
 
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
+    let canvas = document.getElementById('myCanvas');
+    let ctx = canvas.getContext('2d');
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.font = '20px Arial';
@@ -34,7 +31,10 @@ document.getElementById('imcForm').addEventListener('submit', function (event) {
     ctx.fillText('Su IMC es: ' + imc, 10, 30);
 });
 
-
-function validateNumber(input) {
-    return /^\d+$/.test(input);
+function validateNumber(input, min, max) {
+    if (!/^\d+(\.\d+)?$/.test(input)) {
+        return false; // Vérifie si c'est un nombre valide
+    }
+    let num = parseFloat(input);
+    return num >= min && num <= max; // Vérifie si le nombre est dans la plage spécifiée
 }
